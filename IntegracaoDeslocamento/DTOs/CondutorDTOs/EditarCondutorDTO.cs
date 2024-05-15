d963633d2574a393d1491a9b300bafae9ec01e21
Juan Pablo Latreille <juanpablolatreille2015@outlook.com>
2024-05-15 00:12:44 -0300

    ajuste dto

--- IntegracaoDeslocamento/DTOs/CondutorDTOs/EditarCondutorDTO.cs
+++ IntegracaoDeslocamento/DTOs/CondutorDTOs/EditarCondutorDTO.cs
@@ -5,17 +5,11 @@ public class EditarCondutorDTO
     public EditarCondutorDTO(int id, string nome, string numeroHabilitacao, string categoriaHabilitacao, DateTime vencimentoHabilitacao)
     {
         Id = id;
-        Nome = nome;
-        NumeroHabilitacao = numeroHabilitacao;
         CategoriaHabilitacao = categoriaHabilitacao;
         VencimentoHabilitacao = vencimentoHabilitacao;
     }
 
     public int Id { get; set; }
-
-    public string Nome { get; set; }
-
-    public string NumeroHabilitacao { get; set; }
 
     public string CategoriaHabilitacao { get; set; }
 
